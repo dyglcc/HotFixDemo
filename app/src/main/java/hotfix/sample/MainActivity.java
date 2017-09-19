d4595d40323e0afd8bbfcc5f03c03f1e8b437189
dong yuangui <513962116@qq.com>
2017-09-20 00:14:45 +0800

    本地repo plugin创建成功

--- app/src/main/java/hotfix/sample/MainActivity.java
+++ app/src/main/java/hotfix/sample/MainActivity.java
@@ -2,6 +2,7 @@ package hotfix.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import hotfix.sample.com.R;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sample);
-//        System.out.println(100 / 0);
+        System.out.println(100 / 1);
+        Toast.makeText(this, "i am patch", Toast.LENGTH_LONG).show();
     }
 }
