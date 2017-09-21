2ee414eddb41656653c56560a1c7eca0811abef0
dong yuangui <513962116@qq.com>
2017-09-21 14:40:27 +0800

    去掉patchsdk包

--- app/src/main/java/hotfix/sample/MainActivity.java
+++ app/src/main/java/hotfix/sample/MainActivity.java
@@ -2,6 +2,7 @@ package hotfix.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import hotfix.sample.com.R;
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_sample);
         System.out.println(100 / 1);
         Toast.makeText(this, "i am patch", Toast.LENGTH_LONG).show();
+        findViewById(R.id.toastInfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "patch", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
